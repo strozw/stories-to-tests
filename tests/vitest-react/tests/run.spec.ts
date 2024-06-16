73261c5a36c8b8e4057ae9c3700232765faf118f
strozw <strozw@users.noreply.github.com>
2024-06-16 15:23:58 +0900

    chore: code format

--- tests/vitest-react/tests/run.spec.ts
+++ tests/vitest-react/tests/run.spec.ts
@@ -3,11 +3,11 @@ import { describe, it } from "vitest";
 describe.sequential("run", () => {
   it.todo(
     "should be generate test files next to the test subject files and then can be run those tests",
-    () => { },
+    () => {},
   );
 
   it.todo(
     "should be generate test files to `ct` directory and then can be run those tests",
-    () => { },
+    () => {},
   );
 });
